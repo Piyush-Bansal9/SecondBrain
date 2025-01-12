df331455ffbd053cd6f439b597b643d43a82b928
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-01-12 15:56:00 +0530

    Creating Card Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,17 @@ function App() {
 
   return (
     <>
-    <div >
-      <Button icon={<ShareIcon size="md"/>} variant = "secondary" text="Share Brain" />
-      <Button icon={<PlusIcon size="md"/>} variant = "primary" text="Add Content"/>
-      <Card/>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button icon={<ShareIcon size="md"/>} variant = "secondary" text="Share Brain" />
+        <Button icon={<PlusIcon size="md"/>} variant = "primary" text="Add Content"/>
+      </div>
+      <div className="flex gap-2 items-start">
+        <Card title="First tweet" type="tweet" link="https://x.com/elonmusk/status/1878339006171083214"/>
+        <Card title="First yt video" type="youtube"/>
+        {/* <Card title="Some notes" type="document"/> */}
+      </div>
+        
     </div>
     </>
   )
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,5 +1,52 @@
-export default function Card(){
-    return <div className="bg-white p-6 rounded-md border border-slate-300">
-        hi there
+import { DeleteIcon } from "../icons/DeleteIcon";
+import { DocumentIcon } from "../icons/DocumentIcon";
+import { InstaIcon } from "../icons/InstaIcon";
+import { PlayIcon } from "../icons/PlayIcon";
+import { ShareIcon } from "../icons/ShareIcon";
+import { TwitterIcon } from "../icons/TwitterIcon";
+
+interface CardProps{
+    title: string;
+    type: "document" | "tweet" | "youtube" | "instapost";
+    link?: string;
+}
+
+const typeStyles = {
+    "document": <DocumentIcon/>,
+    "tweet": <TwitterIcon/>,
+    "youtube": <PlayIcon/>,
+    "instapost": <InstaIcon/>
+}
+
+// const typeContentStyles = {
+//     "tweet": <blockquote className="twitter-tweet"> 
+//     <a href="https://twitter.com/TwitterDev/status/1234567890123456789"></a>
+//     </blockquote>,
+//     "youtube": 
+// }
+
+export default function Card(props: CardProps){
+    return <div className="bg-white p-4 rounded-md border border-slate-300 max-w-72 min-h-48 min-w-72">
+            <div className="flex justify-between ">
+                <div className="flex items-center gap-2">
+                    <div className="pr-2 text-gray-500">
+                    {typeStyles[props.type]}
+                    </div>
+                    {props.title}
+                </div>
+                <div className="flex items-center gap-2">
+                    <div className="pr-2 text-gray-500">
+                    {<ShareIcon size="md"/>}
+                    </div>
+                    <div className="text-gray-500">
+                    {<DeleteIcon size="md"/>}
+                    </div>
+                </div>
+            </div>
+            <div className="pt-4 ">
+            {props.type==="youtube" && <iframe  className="w-full " width="560" height="315" src="https://www.youtube.com/embed/z7Uv_A4bG-U?si=08AfhJ0YgPtKqe91" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+            // picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+            {props.type==="tweet" && <blockquote className="w-full twitter-tweet"> <a href={props.link?.replace("x.com", "twitter.com")}></a></blockquote>}
+            </div>
     </div>
 }
